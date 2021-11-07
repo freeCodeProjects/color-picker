94a97c0adf1b0d2db510c6743feb85889f377b63
Akash187 <kumarakash007@gmail.com>
2021-11-07 18:06:16 +0530

    style: decreased color card ellipsis max-width

--- src/components/palette/PaletteColorCard.vue
+++ src/components/palette/PaletteColorCard.vue
@@ -5,7 +5,7 @@
 			:style="`background-color: ${color[colorFormat]}`"
 		>
 			<div class="color__name" :style="`color: ${color[colorFormat]}`">
-				<n-ellipsis style="max-width: 15rem">{{ color.name }}</n-ellipsis>
+				<n-ellipsis style="max-width: 12rem">{{ color.name }}</n-ellipsis>
 			</div>
 			<div
 				class="color__action fade-animation-parent cursor-pointer"
